24fb40a0a52e99c37bbbdff2677e4e672a591927
Geshu0126 <geshuzZZ@163.com>
2020-11-28 17:54:25 +0800

    some learn code

---
+++ hello-mybatis-dao/src/main/java/com/shuker/pojo/Student.java
@@ -0,0 +1,15 @@
+package com.shuker.pojo;
+
+/**
+ * @ClassName Student
+ * @Description TODO
+ * @Author Shuker
+ * @Date 2020/11/28 17:53
+ * @Version 1.0
+ **/
+public class Student {
+    private Integer id ;
+    private  String name ;
+    private char gende ;
+
+}
